9861d7bc80bf97793d6c029f1f8a28e4da459909
Christopher Harrison <chrhar@microsoft.com>
2019-06-09 16:47:30 -0700

    Cleanup

--- dates/get_current_date.py
+++ dates/get_current_date.py
@@ -1,7 +1,7 @@
 #To get current date and time we need to use the datetime library
-import datetime
+from datetime import datetime
 
-current_date = datetime.datetime.now()
+current_date = datetime.now()
 # The now function returns current date and time as a datetime object
 
 # You must convert the datetime object to a string
--- dates/input_date.py
+++ dates/input_date.py
@@ -1,12 +1,11 @@
-import datetime
+from datetime import datetime, timedelta
 
 birthday = input('When is your birthday (dd/mm/yyyy)? ')
 
-birthday_date = \
-    datetime.datetime.strptime(birthday, '%d/%m/%Y')
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
 
 print ('Birthday: ' + str(birthday_date))
 
-one_day = datetime.timedelta(days=1)
+one_day = timedelta(days=1)
 birthday_eve = birthday_date - one_day
 print('Day before birthday: ' + str(birthday_eve))
