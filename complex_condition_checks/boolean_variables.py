6974db2f0f40a3e8824179efb849b6ccde6b3dc2
Susan Ibach <8863823+hockeygeekgirl@users.noreply.github.com>
2019-06-20 16:40:38 -0400

    updates to code samples in complex conditions

--- complex_condition_checks/boolean_variables.py
+++ complex_condition_checks/boolean_variables.py
@@ -2,6 +2,7 @@
 gpa = float(input('What was your Grade Point Average? '))
 lowest_grade = float(input('What was your lowest grade? '))
 
+# Boolean variables allow you to remember a True/False value
 if gpa >= .85 and lowest_grade >= .70:
 	honour_roll = True
 else:
